cd1ee3d52b4ce9e4f21abec60a6cfcadc93e76ca
Rodion <george.t.o.p@mail.ru>
2020-01-16 16:22:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android121/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.android121;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView name;
+    TextView mail;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView conclusion =  findViewById(R.id.conclusion);
+        final TextView name =  findViewById(R.id.name);
+        final TextView mail =  findViewById(R.id.mail);
+        TextView btnOk = findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint( "SetTextI18n" )
+            @Override
+            public void onClick(View view) {
+                conclusion.setText("Подписка на рассылку успешно оформлена для пользователя"
+                        +name+ "на электронный адрес"+mail);
+
+            }
+        });
+        TextView btnRemove = findViewById(R.id.btnRemove);
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mail.setText(null);
+                name.setText(null);
+                conclusion.setText(null);
+
+    }
+
+     /*TextView conclusion =  findViewById(R.id.conclusion);
+     TextView name =  findViewById(R.id.name);
+     TextView mail =  findViewById(R.id.mail);
+    public void onClickButtonOk (View view){
+        TextView btnOk = findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint( "SetTextI18n" )
+            @Override
+            public void onClick(View view) {
+                conclusion.setText("Подписка на рассылку успешно оформлена для пользователя"
+                        +name+ "на электронный адрес"+mail);
+
+            }
+        });
+
+    }*/
+    /*public void onClickButtonRemove (View view){
+        TextView btnRemove = findViewById(R.id.btnRemove);
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mail.setText(null);
+                name.setText(null);
+                conclusion.setText(null);
+
+
+            }
+        });
+    }*/
+
+});
+
+}}
